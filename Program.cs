8026464835bfbbffa02c81c0853ef5fa6ef426ed
Roodewald <pavel999977@gmail.com>
2022-11-04 14:21:31 +0500

    v0.01

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
+string[,] dbase = new string[200,5];
 var botClient = new TelegramBotClient("5522766988:AAEK8OThHKWoDc7A7ZBMFson5XjhMy__XDM");
-
 using var cts = new CancellationTokenSource();
+
 
 // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
 var receiverOptions = new ReceiverOptions
@@ -15,13 +16,14 @@ var receiverOptions = new ReceiverOptions
 	AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
 };
 botClient.StartReceiving(
-	updateHandler: HandleUpdateAsync,
-	pollingErrorHandler: HandlePollingErrorAsync,
-	receiverOptions: receiverOptions,
-	cancellationToken: cts.Token
+updateHandler: HandleUpdateAsync,
+pollingErrorHandler: HandlePollingErrorAsync,
+receiverOptions: receiverOptions,
+cancellationToken: cts.Token
 );
 
 var me = await botClient.GetMeAsync();
+
 
 Console.WriteLine($"Start listening for @{me.Username}");
 Console.ReadLine();
@@ -29,42 +31,88 @@ Console.ReadLine();
 // Send cancellation request to stop bot
 cts.Cancel();
 
+
+
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
 	var housingKeyboard = new InlineKeyboardMarkup(new[]
-					{
-                        // first row
-                        new []
-						{
-							InlineKeyboardButton.WithCallbackData("Мелик-Карамова", "MK"),
-							InlineKeyboardButton.WithCallbackData("Рабочая 43", "R43"),
-						},
-                        // second row
-                        new []
-						{
-							InlineKeyboardButton.WithCallbackData("Крылова.д 41/1", "Krilova"),
-							InlineKeyboardButton.WithCallbackData("50 ЛетВЛКСМ", "50let"),
-						}
-					});
-
+	{
+    // first row
+    new []
+	{
+		InlineKeyboardButton.WithCallbackData("Мелик-Карамова 4/1", "Мелик-Карамова 4/1"),
+		InlineKeyboardButton.WithCallbackData("Рабочая 43", "Рабочей 43"),
+	},
+    // second row
+    new []
+	{
+		InlineKeyboardButton.WithCallbackData("Крылова.д 41/1", "\"Крылова.д 41/1"),
+		InlineKeyboardButton.WithCallbackData("50 ЛетВЛКСМ", "50 ЛетВЛКСМ"),
+	}
+	});
+	
 
 	if (update.Type == UpdateType.Message)
 	{
+
 		Message message = update.Message;
 		if (message.Text.ToLower().StartsWith("/start"))
 		{
-			await botClient.SendTextMessageAsync(message.Chat.Id, "Этот бот создан для создания запросов в Техническую службу. \nВыбирете ваш корпус", replyMarkup: housingKeyboard);
+			await botClient.SendTextMessageAsync(message.Chat.Id, "Этот бот создан для создания запросов в Техническую службу. \nВыберете ваш корпус", replyMarkup: housingKeyboard);
 		}
+		if (message.Text.ToLower().StartsWith("/cab"))
+		{
+			dbase[ChekID(message.From.Id), 2] = message.Text;
+			await botClient.SendTextMessageAsync(message.Chat.Id, "Чем может помочь вам тех служба?\nДля отправки используйте команду/task\nПример:/task 5 компьютер не работает сеть");
+		}
+		if (message.Text.ToLower().StartsWith("/task"))
+		{
+			dbase[ChekID(message.From.Id), 3] = message.Text;
+			await botClient.SendTextMessageAsync(message.Chat.Id, "Как к Вам можно обращаться? \nДля отправки используйте команду/name\nПример:/name Павел Петрович ");
+		}
+		if (message.Text.ToLower().StartsWith("/name"))
+		{
+			dbase[ChekID(message.From.Id), 4] = message.Text;
+			await botClient.SendTextMessageAsync(message.Chat.Id, UserData(message.Chat.Id));
+			Console.WriteLine(UserData(message.Chat.Id));
+		}
+		return;
 	}
 
 	if (update.Type == UpdateType.CallbackQuery)
 	{
 		CallbackQuery callbackQuery = update.CallbackQuery;
-		Console.WriteLine($"Received a  {callbackQuery.Data}");
+
+		dbase[ChekID(callbackQuery.From.Id), 1] = callbackQuery.Data;
+		await botClient.SendTextMessageAsync(callbackQuery.From.Id, "В каком кабинете требуется обслуживание?\nДля отправки используйте команду/cab\nПример:/cab 21");
 	}
 }
 
+string UserData(long senderId)
+{
+	long ID = ChekID(senderId);
+	return $"Пользователь {dbase[ID,4]} в корпусе на {dbase[ID,1]}, в кабинете: {dbase[ID, 2]}, \nОставил сообщение:\n{dbase[ID, 3]}";
+}
 
+int ChekID(long senderId)
+{
+	for (int i = 0; i < 200; i++)
+	{
+		if (dbase[i,0] == senderId.ToString())
+		{
+			return i;
+		}
+	}
+	for (int i = 0; i < 200; i++)
+	{
+		if (dbase[i,0] == null)
+		{
+			dbase[i,0] = senderId.ToString();
+			return i;
+		}
+	}
+	return 0;
+}
 
 Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
 {
